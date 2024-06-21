5041abf4695168f9828cc1a0756f217594093012
wonjune <wonjune0426@naver.com>
2024-06-21 13:02:18 +0900

    initial commit

---
+++ src/main/java/com/example/Monetization/System/MonetizationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Monetization.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonetizationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonetizationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Monetization/System/MonetizationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Monetization.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonetizationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
