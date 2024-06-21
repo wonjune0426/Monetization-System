cac1a4eb7071de1b493a28e2e1459c4be3b7c679
wonjune <wonjune0426@naver.com>
2024-06-21 15:06:30 +0900

    github action test

--- src/test/java/com/example/Monetization/System/MonetizationSystemApplicationTests.java
+++ src/test/java/com/example/Monetization/System/MonetizationSystemApplicationTests.java
@@ -1,9 +1,8 @@
 package com.example.Monetization.System;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class MonetizationSystemApplicationTests {
 
 	@Test
